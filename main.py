d1194d074719baa4f9872535a76b7ee1c65fe362
Polina <ptukhap@gmail.com>
2021-11-09 13:55:41 +0200

    add getData

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
+import codecs
+import json
+import os
 
 
-# Press the green button in the gutter to run the script.
+def getData(file):
+    """
+    get data from files
+    :param file: list with names of json files
+    :return train_data_inside, train_data_outside, test_data:
+    numpy arrays of data in first class, second class and test data
+    """
+    script_dir = os.path.dirname(__file__)
+    file_path_1 = os.path.join(script_dir, file[0])
+    file_path_2 = os.path.join(script_dir, file[1])
+    file_path_1 = os.path.normpath(file_path_1)
+    file_path_2 = os.path.normpath(file_path_2)
+    obj1 = codecs.open(file_path_1, 'r', encoding='utf-8').read()
+    obj2 = codecs.open(file_path_2, 'r', encoding='utf-8').read()
+    train_data_inside = np.array(json.loads(obj1)['inside'])  # k = 1
+    train_data_outside = np.array(json.loads(obj1)['outside'])  # k = 0
+    test_data_inside = np.array(json.loads(obj2)['inside'])
+    test_data_outside = np.array(json.loads(obj2)['outside'])
+    return train_data_inside, train_data_outside, test_data_inside, test_data_outside
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    files = ['train_01.json', 'train_02.json']
+    train_first, train_second, test_first, test_second = getData(files)
+    print(train_first)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
