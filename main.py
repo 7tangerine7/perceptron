4589bb86b77e910bd7bcab1eef7b72210c2fde6c
Polina <ptukhap@gmail.com>
2021-11-09 16:11:36 +0200

    add vectors_change

--- main.py
+++ main.py
@@ -25,9 +25,37 @@ def get_data(file):
     return train_data_inside, train_data_outside, test_data_inside, test_data_outside
 
 
+def vectors_change(first, second):
+    """
+    :param first: array from 'inner' part
+    :param second: array from 'outer' part
+    :return: train_data
+    """
+    f = len(first)
+    s = len(second)
+    data = np.ones((f + s, 8))
+    data[:f, 1], data[f:, 1] = first[:, 0], second[:, 0]
+    data[:f, 2], data[f:, 2] = first[:, 1], second[:, 1]
+    data[:f, 3], data[f:, 3] = first[:, 0] ** 2, second[:, 0] ** 2
+    data[:f, 4], data[f:, 4] = first[:, 0] * first[:, 1], second[:, 0] * second[:, 1]
+    data[:f, 5], data[f:, 5] = data[:f, 4], data[f:, 4]
+    data[:f, 6], data[f:, 6] = first[:, 1] ** 2, second[:, 1] ** 2
+    data[f:, 7] = 2  # 8th element for class
+    return data
+
+
+def perceptron(first, second):
+    """
+    learning parameters
+    """
+    parameter = np.zeros(7)
+
+
 if __name__ == '__main__':
     files = ['train_01.json', 'train_02.json']
-    train_first, train_second, test_first, test_second = getData(files)
-    print(train_first)
+    train_first, train_second, test_first, test_second = get_data(files)
+    train_data = vectors_change(train_first, train_second)
+    print(train_data)
+
 
 
