6f1ba22d0794c16daec7201c837013da7f691992
Polina <ptukhap@gmail.com>
2021-11-09 23:01:44 +0200

    find parameters with checking possible problems

--- main.py
+++ main.py
@@ -44,11 +44,35 @@ def vectors_change(first, second):
     return data
 
 
-def perceptron(data):
+def bounders(w, v):
+    idx = np.where(w <= 0)
+    a = np.array([])
+    for i in idx[0]:
+        a = np.append(a, [0, 0, 0, v[i, 0]**2, v[i, 1] * v[i, 0], v[i, 1] * v[i, 0], v[i, 1] ** 2, 1])
+    return a
+
+
+def parameters(temp):
+
+    sigma11 = temp[3]
+    sigma12 = temp[4]
+    sigma22 = temp[6]
+
+    sigma_t = np.array([[sigma11, sigma12], [sigma12, sigma22]])
+    determinant = sigma11 * sigma22 - sigma12**2
+
+    m1 = (sigma12 * temp[2] - sigma22 * temp[1]) / determinant
+    m2 = - (temp[2] + 2 * sigma11 * (sigma12 * temp[2] - sigma12 * temp[1])) / (2 * sigma22 * determinant)
+    m_t = np.array([m1, m2])
+
+    theta_t = np.exp(0.5 * (temp[0] + 1 / np.sqrt(determinant) - m_t.T @ sigma_t @ m_t))
+    return m_t, sigma_t, theta_t
+
+
+def perceptron(data, temp):
     """
     learning parameters
     """
-    temp = np.zeros(7)  # vector lambda
     y = data[:, 7] * (data[:, :7] @ temp.T)
     while y.any() <= 0:
         idx = np.where(y == 0)
@@ -60,11 +84,17 @@ def perceptron(data):
 
 if __name__ == '__main__':
     files = ['train_01.json', 'train_02.json']
+
     train_first, train_second, test_first, test_second = get_data(files)
     train_data = vectors_change(train_first, train_second)
-    param = perceptron(train_data)
-    print(param)
 
+    param = np.zeros(7)  #plak-plak
+    param = perceptron(train_data, param)
+    m, sigma, theta = parameters(param)
+    values, vectors = np.linalg.eig(sigma)
+    train_data = np.vstack((train_data, bounders(values, vectors)))  # find vector to add
+    param = perceptron(train_data, param)  # add vector for correct work
+    print(param)
 
 
 
