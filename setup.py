499a3d0d50b9d3ce875eecb9ff922f879fd73482
7tangerine7 <48069220+7tangerine7@users.noreply.github.com>
2021-11-13 14:48:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ setup(
     name="perceptron",
     version="1.0",
     author="Polina",
-    description="Little death. No README. No LICENSE.",
+    description="._.",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: what is license?",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "matplotlib"],
-    entry_points={"console_scripts": ["perceptron = perceptron-code.main:main"]},
+    install_requires=["numpy", "matplotlib"]},
 )
